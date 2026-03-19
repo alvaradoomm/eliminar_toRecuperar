8235ae9cd0a4706637dcc78177efdb47a649e62c
Marisol Alvarado <alvacamarisol06@gmail.com>
2026-03-18 22:32:43 -0600

    Add files via upload

---
+++ eliminar.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+function ayuda {
+echo "
+---AYUDA---
+eliminar.sh ruta/archivo 
+Borra un archivo de forma que puede ser recuperado
+Parametros: 
+- ruta/archivo: ruta absuluta del archivo que desea borrar"
+}
+
+#validar entrada
+if [ $# -lt 1 ]; then
+        echo "Debes pasar un parametro"
+        ayuda
+        exit 1
+fi
+
+archivo="$1"
+
+#validar archivo
+if test -f "$archivo"; then 
+	echo "archivo borrado"
+fi
+
+#"borrar" archivo
+mv "$archivo" /tmp
